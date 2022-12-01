d8b6f239695d809d5a1c1081fcb8eb0ffe9454d6
Dmitry <dmitriy_kozachenko_02@mail.ru>
2022-12-01 16:37:14 +0500

    Козаченко Дмитрий 11/11 (Сливзерин))

--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -3,6 +3,21 @@ const root = document.querySelector(":root");
 const defaultState = root.classList.contains("dark");
 
 themeSwitch.checked = defaultState;
+changeTheme()
 themeSwitch.addEventListener("click", () => {
   root.classList.toggle("dark");
+  document.cookie = `color=${root.classList.value}`
+  console.log(document.cookie)
 });
+
+function changeTheme(){
+  let array = {};
+  let coockiesArray = document.cookie.split(/; /);
+  for (let i = 0, len = coockiesArray.length; i < len; i++) {
+    let cookie = coockiesArray[i].split(/=/);
+    array[cookie[0]] = cookie[1];
+  }
+  if (array.color === 'dark'){
+    root.classList.toggle("dark");
+  }
+}
