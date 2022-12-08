41049f7c4c966d129c54504279205dbd6b77ed5c
Denis-GM <gainutdinov.deniz@yandex.ru>
2022-12-08 14:17:10 +0500

    11

--- index.mjs
+++ index.mjs
@@ -4,8 +4,48 @@ import hbs from "express-handlebars";
 import cookieParser from "cookie-parser";
 
 const rootDir = process.cwd();
-const port = 3000;
+// const port = 3000;
+const port = 3001;
 const app = express();
+
+class Coffee {
+  constructor(name, image, price) {
+    this.name = name;
+    this.image = image;
+    this.price = price;
+  }
+};
+
+class User {
+  constructor(userName){
+    this.userName = userName;
+    this.cart = [];
+    this.costCart = 0;
+    this.historyCart = [];
+  }
+
+  buy (itemName) {
+    for(let i = 0; i < menu.length; i++){
+      if(itemName == menu[i].name) {
+        this.cart.push(menu[i]);
+        this.costCart += menu[i].price;
+      }
+    }
+  }
+}
+
+let users = [];
+let costCart = 0;
+let historyCart = [];
+
+let menu = [ 
+  new Coffee("Americano", "/static/img/americano.jpg", 180),
+  new Coffee("Cappuccino", "/static/img/cappuccino.jpg", 200),
+  new Coffee("Espresso", "/static/img/espresso.jpg", 220),
+  new Coffee("Flat White", "/static/img/flat-white.jpg", 205),
+  new Coffee("Latte Macchiato", "/static/img/latte-macchiato.jpg", 230),
+  new Coffee("Latte", "/static/img/latte.jpg", 210),
+]
 
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
@@ -20,38 +60,86 @@ app.engine(
   })
 );
 
+app.use(cookieParser());
+app.use('/static' ,express.static('static'));
+
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+  
+  res.redirect('/menu');
 });
 
 app.get("/menu", (_, res) => {
   res.render("menu", {
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
+    title: 'Menu',
+    items: menu,
   });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  let currentUser = req.cookies.userName;
+  console.log(currentUser);
+  // console.log(users);
+  if(currentUser === undefined || currentUser === 'Аноним'){
+    res.redirect('/login');
+  }
+  else {
+    for(let i = 0; i < menu.length; i++){
+      if(req.params.name == menu[i].name) {
+        // if(users[currentUser].length) users[currentUser][0] = 0;
+        // users[currentUser][0] += menu[i].price
+        users[currentUser].push(menu[i]);
+      }
+    }
+  }
+  console.log(users);
+  res.redirect('/menu');
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+  let currentUser = req.cookies.userName;
+  res.render("cart", {
+    layout: "default",
+    title: 'Cart',
+    items: users[currentUser],
+    cost: costCart,
+  });
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  cart = [];
+  costCart = 0;
+  res.redirect('/cart');
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+  let currentUser = req.query.username || req.cookies.username ||  "Аноним";
+
+  res.cookie("userName", currentUser);
+  checkAuthorization(currentUser);
+  console.log(currentUser);
+
+  res.render("login", {
+    layout: "default",
+    userName: currentUser,
+    title: 'Личный кабинет'
+  });
 });
 
+app.get("/history", (req, res) => {
+  let currentUser = req.cookies.userName;
+
+  res.render("history", {
+    layout: "default",
+    history: historyCart[currentUser],
+    title: 'История заказов'
+  });
+
+});
+
+function checkAuthorization(userName) {
+  if(users[userName] === undefined){
+    users[userName]=[];
+  }
+}
 app.listen(port, () => console.log(`App listening on port ${port}`));
--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -1,8 +1,14 @@
 const themeSwitch = document.querySelector(".theme__switch input");
 const root = document.querySelector(":root");
 const defaultState = root.classList.contains("dark");
+// localStorage.setItem('theme', 'light');
 
 themeSwitch.checked = defaultState;
+root.classList.value = localStorage.getItem('theme');
 themeSwitch.addEventListener("click", () => {
-  root.classList.toggle("dark");
+  // localStorage.theme = 'dark';
+  root.classList.toggle('dark');
+  localStorage.theme = root.classList.value;
+  // root.classList.toggle(localStorage.theme);
+  // alert( localStorage.getItem('theme') );
 });
