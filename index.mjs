66015fc9b7b647606a584fef9052c8eb0e503b06
rustam <rustamskysan@gmail.com>
2022-12-13 17:43:17 +0500

    task done!!!

--- index.mjs
+++ index.mjs
@@ -6,6 +6,45 @@ import cookieParser from "cookie-parser";
 const rootDir = process.cwd();
 const port = 3000;
 const app = express();
+let cartKorz=[];
+let history = []
+
+app.use(cookieParser());
+app.use("/static", express.static("static"))
+
+const products  = [
+  {
+    name: "Americano",
+    image: "/static/img/americano.jpg",
+    price: 777,
+  },
+  {
+    name: "Cappuccino",
+    image: "/static/img/cappuccino.jpg",
+    price: 333
+  },
+  {
+    name: "Espresso",
+    image: "/static/img/espresso.jpg",
+    price: 222
+  },
+  {
+    name: "Latte",
+    image: "/static/img/latte.jpg",
+    price: 111
+  },
+  {
+    name: "Flat white",
+    image: "/static/img/flat-white.jpg",
+    price: 666
+  },
+  {
+    name: "Latte macchiato",
+    image: "/static/img/latte-macchiato.jpg",
+    price: 228
+  },
+]
+
 
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
@@ -21,37 +60,76 @@ app.engine(
 );
 
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+ res.redirect('/menu');
 });
 
 app.get("/menu", (_, res) => {
   res.render("menu", {
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
+    items:products,
+    title:'Меню'
   });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  const username = getUsername(req);
+
+  if (cartKorz[username] === undefined) cartKorz[username] = [];
+  cartKorz[username].push(products.find(product =>product.name === req.params.name));
+  console.log(cartKorz[username])
+  res.redirect("/menu");
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+  const username = getUsername(req);
+  console.log(cartKorz)
+  console.log(cartKorz[username])
+  if (cartKorz[username] === undefined) cartKorz[username] = [];
+  res.render("cart", {
+    layout: "default",
+    cartKorz: cartKorz[username],
+    sum: cartKorz[username].reduce((sum, item) => sum + item.price, 0),
+    title:"Корзина"
+  })
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  const username = getUsername(req);
+  if (history[username] === undefined) history[username] = [];
+  if (cartKorz[username].length > 0) {
+    history[username].push({
+      items: cartKorz[username],
+      totalPrice: cartKorz[username].reduce((sum, item) => sum + item.price, 0),
+    });
+    cartKorz[username] = []
+  }
+  if (cartKorz[username] !== undefined) cartKorz[username] = [];
+  res.redirect("/cart");
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+  const username = req.query.username || getUsername(req);
+  res.cookie("username", username)
+  res.render("login", {
+    layout: "default",
+    username: username,
+    title:'Регистрация'
+    }
+  )
 });
+
+app.get("/history", (req, res) => {
+  const username = getUsername(req);
+  if (history[username] === undefined) history[username] = [];
+  res.render("history", {
+    layout: "default",
+    history: history[username],
+    title: "История",
+  })
+});
+
+function getUsername(req) {
+  return req.cookies.username || "Аноним"
+}
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -3,6 +3,21 @@ const root = document.querySelector(":root");
 const defaultState = root.classList.contains("dark");
 
 themeSwitch.checked = defaultState;
+swap()
 themeSwitch.addEventListener("click", () => {
-  root.classList.toggle("dark");
+    root.classList.toggle("dark");
+    document.cookie = `color=${root.classList.value}`
+    console.log(document.cookie)
 });
+
+function swap() {
+    let obj = {};
+    let cookies = document.cookie.split(/; /);
+    for (let i = 0, len = cookies.length; i < len; i++) {
+        let cookie = cookies[i].split(/=/);
+        obj[cookie[0]] = cookie[1];
+    }
+    if (obj.color == 'dark') {
+        root.classList.toggle("dark");
+    }
+}
