5e2f4646ca2090b97fec94db841899a5bc07baa4
Степан <elkinstepan405@gmail.com>
2023-11-30 14:40:08 +0500

    Task Done

--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -3,6 +3,9 @@ const root = document.querySelector(":root");
 const defaultState = root.classList.contains("dark");
 
 themeSwitch.checked = defaultState;
+root.classList.value = localStorage.getItem("theme");
+
 themeSwitch.addEventListener("click", () => {
   root.classList.toggle("dark");
+  localStorage.setItem("theme", root.classList.value);
 });
